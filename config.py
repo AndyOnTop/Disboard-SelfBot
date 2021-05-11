eb95182549995233a2b0091e25d74a199bb80724
AndyOnTop <80862417+AndyOnTop@users.noreply.github.com>
2021-05-11 17:33:11 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+### config file ###
+token = "" # Put your discord token here.
+preifx = ";" # Put the prefix you want here.
+wait_time = "7460" # Put the wait time in seconds. !!!Dont change this unless you know what you're doing.!!!
+disboard_prefix = "!d" # Put the disboard prefix here no spaces.
+stream = True # Have a streaming status to my youtube video.
