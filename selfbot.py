51fdccd0ed2bbc66d244059fbcb1a91022f03016
AndyOnTop <80862417+AndyOnTop@users.noreply.github.com>
2021-05-11 17:33:55 -0500

    Add files via upload

--- selfbot.py
+++ selfbot.py
@@ -1,22 +1,49 @@
-from andyontop import *
-from discord.ext import commands
-import discord
-import time
+try:
+    from discord.ext import commands
+    import discord
+    import asyncio
+    from time import sleep
+    from random import randint
+    import config
+    from os import system
+except ImportError:
+    print("Trying to install the required modules! THIS MAY DISPLAY LARGE ERRORS!\nPlease try to run this script again once all of the modules have been successfully installed.\n")
+    input("Press enter to start installing... ")
+    system("py -m pip install -r requirements.txt")
+    system("python -m pip install -r requirements.txt")
+    system("python3 -m pip install -r requirements.txt")
+    input("\n\nDone installing modules! please restart the script now. Press enter to continue... ")
+    quit()
 
-token = ""
-bot = commands.Bot("/--/", self_bot=True)
+bot = commands.Bot(config.preifx, self_bot=True)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ducchub")) # You can delete this line or change ducchub to your server name, its just a custom status
-    andyontop = ascii('Andy On Top')
-    print(andyontop)
+    print("""
+           _   _ _______     __   ____  _   _   _______ ____  _____  
+     /\   | \ | |  __ \ \   / /  / __ \| \ | | |__   __/ __ \|  __ \ 
+    /  \  |  \| | |  | \ \_/ /  | |  | |  \| |    | | | |  | | |__) |
+   / /\ \ | . ` | |  | |\   /   | |  | | . ` |    | | | |  | |  ___/ 
+  / ____ \| |\  | |__| | | |    | |__| | |\  |    | | | |__| | |     
+ /_/    \_\_| \_|_____/  |_|     \____/|_| \_|    |_|  \____/|_|    
+                            Credits:
+                       Andy: (andylol#1234)
+              Thanks to omens for anti selfbot detection
+    """)
+    if config.stream:
+        await bot.change_presence(activity=discord.Streaming(name="Andy On Top", url=""))
 
 @bot.command()
 async def bump(ctx):
-    print("Starting")
-    while True:
-        await ctx.send('!d bump')
-        time.sleep(7260)
+    print('Auto bump started')
 
-bot.run(token, bot=False)
+    async def sendMsg(msg):
+        async with ctx.typing():
+            await asyncio.sleep(randint(3, 7))
+        await ctx.send(msg)
+
+    while True:
+        await sendMsg(config.disboard_prefix + " bump")
+        sleep(int(config.wait_time))
+
+bot.run(config.token, bot=False)
